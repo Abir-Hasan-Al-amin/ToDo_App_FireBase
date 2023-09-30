393d675a27cea96c7bb5dafb6e63daedf3d6ae00
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2023-09-30 21:27:47 +0600

    Add and Delete All Functionality Added

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,6 @@
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-    // Import Firebase Authentication and Realtime Database
-import { getAuth, signOut }  from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getDatabase, ref, push, onValue, set } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
@@ -21,13 +16,110 @@ import { getAuth, signOut }  from "https://www.gstatic.com/firebasejs/10.4.0/fir
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
+    const database = getDatabase(app);
 
-    const out=document.getElementById('out');
-    out.addEventListener('click',()=>{
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            window.location.href = 'index.html';
-            }).catch((error) => {
-            // An error happened.
+    //log out
+    const out = document.getElementById('out');
+    out.addEventListener('click', () => {
+        signOut(auth)
+            .then(() => {
+                window.location.href = 'index.html';
+            })
+            .catch((error) => {
+                console.error("Error signing out: ", error);
             });
     });
+
+    // code 
+    function addTaskToDatabase(userUid, title) {
+        const userTasksRef = ref(database, `users/${userUid}/tasks`);
+        const newTaskRef = push(userTasksRef);
+    
+        const taskData = {
+            title: title,
+        };
+    
+        set(newTaskRef, taskData)
+            .then(() => {
+                console.log("Task added successfully to Firebase Realtime Database");
+            })
+            .catch((error) => {
+                console.error("Error adding task to Firebase Realtime Database: ", error);
+            });
+    }
+    const addList = document.querySelector('.addList');
+    function renderTasks(tasks) {
+        addList.innerHTML = '';
+    
+        for (const taskId in tasks) {
+            const task = tasks[taskId];
+            const taskHtml = `
+                <section class="list">
+                    <section class="info">
+                        <section class="checkBox">
+                        </section>
+                        <p>${task.title}</p>
+                    </section>
+                    <section class="delAndedit">
+                        <section class="editList">
+                            <i class="fa-regular fa-pen-to-square"></i>
+                        </section>
+                        <section class="delList">
+                            <i class="fa-solid fa-trash-can"></i>
+                        </section>
+                    </section>
+                </section>
+            `;
+            addList.innerHTML += taskHtml;
+        }
+    }
+
+const addButton = document.querySelector('.addTodo button');
+addButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    const addTaskInput = document.getElementById('addTask');
+    const title = addTaskInput.value;
+    if (title.trim() !== '') {
+        const user = auth.currentUser;
+        if (user) {
+            const userUid = user.uid;
+            addTaskToDatabase(userUid, title);
+            addTaskInput.value = '';
+        } else {
+            console.error("User not authenticated");
+        }
+    }
+});
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        const userUid = user.uid;
+        const userTasksRef = ref(database, `users/${userUid}/tasks`);
+        onValue(userTasksRef, (snapshot) => {
+            const tasks = snapshot.val();
+            if (tasks) {
+                renderTasks(tasks);
+            }
+        });
+    } else {
+        console.log("User is not signed in.");
+    }
+});
+
+const deleteAllButton = document.querySelector('.delButton button');
+deleteAllButton.addEventListener('click', function () {
+    const user = auth.currentUser;
+    if (user) {
+        const userUid = user.uid;
+        const userTasksRef = ref(database, `users/${userUid}/tasks`);
+        set(userTasksRef, null)
+            .then(() => {
+                console.log("All tasks deleted successfully.");
+                addList.innerHTML = '';
+            })
+            .catch((error) => {
+                console.error("Error deleting tasks: ", error);
+            });
+    } else {
+        console.error("User not authenticated");
+    }
+});
